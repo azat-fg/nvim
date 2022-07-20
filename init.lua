30c56ca09d401652db6c9cd7dde01fb6869d4f74
Azat Khasanov <aza8048@yandex.ru>
2022-07-20 18:21:19 +0300

    Update

--- init.lua
+++ init.lua
@@ -1,16 +1,7 @@
-require "keybindings"
-require "packages"
-require "config"
-require "options"
-require "alpha"
-
-local configs = require 'nvim-treesitter.configs'
-configs.setup {
-  ensure_installed = { "lua", "json", "dockerfile", "bash", "yaml", "python", "go" },
-  highlight = { -- enable highlighting
-    enable = true,
-  },
-  indent = {
-    enable = false, -- default is disabled anyways
-  }
-}
+require "user.keybindings"
+require "user.packages"
+require "user.config"
+require "user.cmp"
+require "user.options"
+require "user.treesitter"
+require "user.lsp"
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,10 @@
+local configs = require 'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = { "lua", "json", "dockerfile", "bash", "yaml", "python", "go" },
+  highlight = { -- enable highlighting
+    enable = true,
+  },
+  indent = {
+    enable = false, -- default is disabled anyways
+  }
+}
